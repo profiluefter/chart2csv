8f50d6ddb00ae4e1fb55ea6658bd2247546d17fc
Fabian <fabian.paul.gurtner@gmail.com>
2021-11-22 11:25:29 +0100

    Initial commit

---
+++ chart2csv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chart2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

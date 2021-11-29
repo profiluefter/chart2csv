be81710f89840e12e6636ad9f999367d9b9e98fd
Fabian <fabian.paul.gurtner@gmail.com>
2021-11-29 11:27:26 +0100

    Read scale

--- chart2csv/Program.cs
+++ chart2csv/Program.cs
@@ -4,12 +4,35 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace chart2csv
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             var image = Image.Load<Rgba32>("chart.png");
-            Console.WriteLine(image[0,0]);
+
+            for (var j = 0; j < image.Height; j++)
+            {
+                var characters = "";
+                for (var i = 0; i < image.Width; i++)
+                {
+                    if (image[i, j].ToHex() == "7D7D7DFF" && image[i - 1, j].ToHex() == "B2B2B2FF")
+                    {
+                        image[i, j] = Rgba32.ParseHex("FF0000FF");
+                        characters += "0";
+                    }
+                    else if (image[i, j].ToHex() == "A0A0A0FF" && image[i - 1, j].ToHex() == "DDDDDDFF")
+                    {
+                        image[i, j] = Rgba32.ParseHex("00FF00FF");
+                        characters += "1";
+                    }
+                }
+
+                if (characters.Length == 0) continue;
+                var number = int.Parse(characters);
+                Console.WriteLine(number);
+            }
+
+            image.Save("output.png");
         }
     }
 }

dd36f11ce7b1832e6bdc512d43d483c73d129200
Fabian <fabian.paul.gurtner@gmail.com>
2021-12-07 13:16:30 +0100

    Replace point processing with average

--- chart2csv/Program.cs
+++ chart2csv/Program.cs
@@ -19,47 +19,15 @@ namespace chart2csv
             var points = Chart2Csv.GetPoints(image, GraphHex)
                 .GroupBy(x => x.X)
                 .OrderBy(x => x.Key)
+                .Select(p => p.Count() == 1
+                    ? p.Single()
+                    : new Point(p.Average(x => x.X), p.Average(x => x.Y)))
                 .ToList();
-
-            var orderedPoints = new List<Point>();
-
-            /*
-             * This loop processes each pair of points that share the same X coordinate (are above each other) and finds
-             * the point that is nearest to the previous and next point. This is done to improve accuracy when
-             * interpolating values to the next or previous point.
-             */
-            for (var index = 0; index < points.Count; index++)
-            {
-                var pointsHere = points[index];
-                if (pointsHere.Count() == 1)
-                {
-                    orderedPoints.Add(pointsHere.Single());
-                    continue;
-                }
-
-                var nearestPointToPrevious = pointsHere
-                    .OrderBy(point => Math.Abs(point.Y - orderedPoints.Last().Y))
-                    .First();
-                orderedPoints.Add(nearestPointToPrevious);
-
-                if (index + 1 == points.Count)
-                    continue;
-                var nextPoints = points[index + 1];
-                if (nextPoints.Count() != 1)
-                    continue;
-                var nextPoint = nextPoints.Single();
-
-                var nearestPointToNext = pointsHere
-                    .OrderBy(point => Math.Abs(point.Y - nextPoint.Y))
-                    .First();
-                if (!nearestPointToNext.Equals(nearestPointToPrevious))
-                    orderedPoints.Add(nearestPointToNext);
-            }
 
             image.Mutate(context => context.DrawLines(
                 Color.ParseHex("FF0000FF"),
                 1f,
-                orderedPoints
+                points
                     .Select(x => new PointF((float)x.X, (float)x.Y))
                     .ToArray()
             ));

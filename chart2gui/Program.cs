1586f01e81d5f7271affd4cc64cda71e273fd4e5
Fabian <fabian.paul.gurtner@gmail.com>
2022-01-24 19:24:08 +0100

    Add SFML GUI

---
+++ chart2gui/Program.cs
@@ -0,0 +1,17 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+var window = new RenderWindow(new VideoMode(1280, 720), "Title ", Styles.Fullscreen);
+
+window.Closed += (_, _) => window.Close();
+
+var chartImage = new Image("charts/00.0-08.0-35.0-35.0-40.0-30.0-01.0-04.0-02.0-NONE.png");
+var sprite = new Sprite(new Texture(chartImage));
+
+while (window.IsOpen)
+{
+    window.DispatchEvents();
+    window.Clear();
+    window.Draw(sprite);
+    window.Display();
+}

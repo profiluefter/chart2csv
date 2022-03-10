72eeb6c7382b292c2708eea93cf54c4401f87f82
Infendro <Schoenberger.Fabian@sus.htl-grieskirchen.at>
2022-03-10 07:41:59 +0100

    Log: add support for next scene
    + add scene to build settings

--- FiveNightsAtTorstens/Assets/Scripts/ShowObjectsAndText.cs
+++ FiveNightsAtTorstens/Assets/Scripts/ShowObjectsAndText.cs
@@ -1,54 +1,67 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
+using Scene = UnityEngine.SceneManagement.Scene;
 
 public class ShowObjectsAndText : MonoBehaviour {
-    private List<GameObject> _initialObjects;
     public string objectTag;
+    private List<GameObject> _initialObjects;
     private List<GameObject> _objects;
     private int _objectsPerText;
     public Color objectColorOnFinish;
     
     public List<string> texts;
-    private List<string> _texts;
     public string lastText;
     public TextMeshPro text;
+
+    private bool _finished;
+    public string nextScene;
     
     // Start is called before the first frame update
     void Start() {
         _initialObjects = GameObject.FindGameObjectsWithTag(objectTag).ToList();
         _objects = new List<GameObject>(_initialObjects);
-
-        _texts = new List<string>(texts);
         
-        _objectsPerText = _objects.Count / (_texts.Count+1);
+        _objectsPerText = _objects.Count / (texts.Count+1);
     }
 
     public void NextObjectsAndText() {
+        if (_finished) {
+            SceneManager.LoadScene(nextScene);
+            return;
+        }
+        
         var objectsPerText = _objectsPerText;
         
-        if (_texts.Count == 0 || _objects.Count == 0) {
-            text.text = lastText;
-            objectsPerText = _objects.Count;
-            
-            foreach (var o in _initialObjects) {
-                o.GetComponent<SpriteRenderer>().color = objectColorOnFinish;
-            }
+        //display text
+        if (texts.Count > 0) {
+            var textIndex = Random.Range(0, texts.Count);
+            text.text = texts[textIndex];
+            texts.RemoveAt(textIndex);
         }
         else {
-            var textIndex = Random.Range(0, _texts.Count);
-            text.text = _texts[textIndex];
-            _texts.RemoveAt(textIndex);
+            text.text = lastText;
+            
+            //set it to show the rest of the objects
+            objectsPerText = _objects.Count;
         }
         
+        //show objects
         for (var i = 0; i < objectsPerText; i++) {
             var objectIndex = Random.Range(0, _objects.Count);
             _objects[objectIndex].GetComponent<SpriteRenderer>().enabled = true;
             _objects.RemoveAt(objectIndex);
+        }
+
+        //finished
+        if (_objects.Count == 0) {
+            foreach (var o in _initialObjects) {
+                o.GetComponent<SpriteRenderer>().color = objectColorOnFinish;
+            }
+            _finished = true;
         }
     }
 }

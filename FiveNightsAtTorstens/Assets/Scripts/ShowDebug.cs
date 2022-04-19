fc4f1c062671796758a87669b6ee1ed9f84125d0
TaWi04 <tamarawimmer04@gmail.com>
2022-04-19 15:41:39 +0200

    Implement Gameplay for DebugScene

---
+++ FiveNightsAtTorstens/Assets/Scripts/ShowDebug.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShowDebug : MonoBehaviour
+{
+    
+    public List<GameObject> Objects;
+
+    private void Awake()
+    {
+        Objects.ToList().ForEach(x => x.SetActive(false));
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Objects[0].SetActive(true);
+        Objects.Remove(Objects[0]);
+    }
+}

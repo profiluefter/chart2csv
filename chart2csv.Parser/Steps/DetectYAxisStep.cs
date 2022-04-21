e761b088dd7eb5cad5a4037f99b366c520695bb2
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2022-04-21 19:15:47 +0200

    Implement windows chart reader
    Closes #8

--- chart2csv.Parser/Steps/DetectYAxisStep.cs
+++ chart2csv.Parser/Steps/DetectYAxisStep.cs
@@ -1,11 +1,15 @@
 using chart2csv.Parser.States;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace chart2csv.Parser.Steps;
 
 public class DetectYAxisStep : ParserStep<ChartOriginState, YAxisState>
 {
-    private static readonly Color NumberZeroColor = Color.ParseHex("7A7A7A");
+    private static readonly Color NumberZeroLinuxColor = Color.ParseHex("7A7A7A");
+    private static readonly Color NumberZeroWindowsColorLeft = Color.ParseHex("B2B2B2");
+    private static readonly Color NumberZeroWindowsColorRight = Color.ParseHex("8E8E8E");
+    
     private static readonly Color LineYMarkerColor = Color.ParseHex("B1B1B1");
     private static readonly Color LineYLabelColor = Color.ParseHex("B2B2B2");
 
@@ -16,46 +20,9 @@ public class DetectYAxisStep : ParserStep<ChartOriginState, YAxisState>
         var origin = input.OriginPoint;
         var image = input.InitialState.InputImage;
         
-        var digits = new Dictionary<int, int>();
+        var digits = FindDigits(origin, image);
 
-        for (var x = 2; x < origin.X; x++)
-        for (var y = 0; y < origin.Y; y++)
-        {
-            if ((Color) image[x - 2, y] != NumberZeroColor || (Color) image[x + 2, y] != NumberZeroColor) continue;
-
-            if (!digits.ContainsKey(y))
-                digits[y] = 0;
-            digits[y]++;
-        }
-
-        var numbers = new Dictionary<int, int>();
-
-        foreach (var (y, count) in digits)
-        {
-            var realY = y;
-            if ((Color) image[origin.X, y] == LineYLabelColor || (Color) image[origin.X, y] == LineYMarkerColor)
-            {
-                // realY is accurate
-            }
-            else if ((Color) image[origin.X, y - 1] == LineYLabelColor || (Color) image[origin.X, y - 1] == LineYMarkerColor)
-            {
-                // line is one above
-                realY--;
-            }
-            else if ((Color) image[origin.X, y + 1] == LineYLabelColor || (Color) image[origin.X, y + 1] == LineYMarkerColor)
-            {
-                // line is one below
-                realY++;
-            }
-            else
-            {
-                throw new Exception($"Couldn't find corresponding graph line for digits at height {y}");
-            }
-
-            var number = (int) Math.Pow(10, count);
-            //Console.WriteLine($"Found number {number} at y-position {y} with real position of {realY}");
-            numbers[realY] = number;
-        }
+        var numbers = DigitsToNumbers(image, origin, digits);
 
         switch (numbers.Count) {
             case > 1: {
@@ -90,7 +57,73 @@ public class DetectYAxisStep : ParserStep<ChartOriginState, YAxisState>
 
         return new YAxisState(input, numbers, GetValue);
     }
-    
+
+    private static Dictionary<int, int> FindDigits(Pixel origin, Image<Rgba32> image)
+    {
+        var linuxDigits = new Dictionary<int, int>();
+        var windowsDigits = new Dictionary<int, int>();
+
+        for (var x = 2; x < origin.X; x++)
+        for (var y = 0; y < origin.Y; y++)
+        {
+            // search for linux pattern
+            if ((Color)image[x - 2, y] == NumberZeroLinuxColor && (Color)image[x + 2, y] == NumberZeroLinuxColor)
+            {
+                if (!linuxDigits.ContainsKey(y))
+                    linuxDigits[y] = 0;
+                linuxDigits[y]++;
+            }
+            
+            // search for windows pattern
+            if ((Color)image[x - 2, y] == NumberZeroWindowsColorLeft &&
+                (Color)image[x + 2, y] == NumberZeroWindowsColorRight)
+            {
+                if (!windowsDigits.ContainsKey(y))
+                    windowsDigits[y] = 0;
+                windowsDigits[y]++;
+            }
+        }
+
+        Console.WriteLine($"Found {linuxDigits.Count} linux digits and {windowsDigits.Count} windows digits");
+        var isWindows = windowsDigits.Count > linuxDigits.Count;
+        Console.WriteLine($"Assuming this is a {(isWindows ? "windows" : "linux")} chart");
+
+        return isWindows ? windowsDigits : linuxDigits;
+    }
+
+    private static Dictionary<int, int> DigitsToNumbers(Image<Rgba32> image, Pixel origin, Dictionary<int, int> digits)
+    {
+        var numbers = new Dictionary<int, int>();
+
+        foreach (var (y, count) in digits)
+        {
+            var realY = y;
+            if ((Color)image[origin.X, y] == LineYLabelColor || (Color)image[origin.X, y] == LineYMarkerColor)
+            {
+                // realY is accurate
+            }
+            else if ((Color)image[origin.X, y - 1] == LineYLabelColor || (Color)image[origin.X, y - 1] == LineYMarkerColor)
+            {
+                // line is one above
+                realY--;
+            }
+            else if ((Color)image[origin.X, y + 1] == LineYLabelColor || (Color)image[origin.X, y + 1] == LineYMarkerColor)
+            {
+                // line is one below
+                realY++;
+            }
+            else
+            {
+                throw new Exception($"Couldn't find corresponding graph line for digits at height {y}");
+            }
+
+            var number = (int)Math.Pow(10, count);
+            numbers[realY] = number;
+        }
+
+        return numbers;
+    }
+
     private static double GetValue(Dictionary<int, int> numbers, double y) {
         if (numbers.Count < 2) throw new Exception("not enough markers to calculate y value"); 
             
--- chart2csv.Test/Program.cs
+++ chart2csv.Test/Program.cs
@@ -2,9 +2,9 @@
 using chart2csv.Parser.States;
 using SixLabors.ImageSharp;
 
-const string chartPath = "charts/00.0-08.0-46.0-35.0-100.0-40.0-01.0-04.0-02.0-NONE.png";
+const string chartPath = "charts/windows-chart.png";
 
 var parser = new SequentialParserExecutor(chartPath);
-File.WriteAllLines("output.csv", parser.ComputeState<CSVState>().CSVLines);
-parser.ComputeState<LineOverlayChartState>().LineOverlayChart.SaveAsPng("output.png");
 parser.ComputeState<PointClusterImageState>().PointClusterOverlayChart.SaveAsPng("output-cluster.png");
+parser.ComputeState<LineOverlayChartState>().LineOverlayChart.SaveAsPng("output.png");
+File.WriteAllLines("output.csv", parser.ComputeState<CSVState>().CSVLines);
